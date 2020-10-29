c17fed3e588432123c310e9c59b61d3b2cf3347e
Dustin Blanton <dustinblntn@gmail.com>
2020-10-29 23:32:43 +0000

    Automatically backed up by Learn

---
+++ looping.rb
@@ -0,0 +1,7 @@
+def busloop
+  loop do
+    puts "Never pass a school bus if it has flashing red lights"
+  end
+end
+
+busloop
---
+++ variables.rb
@@ -0,0 +1,9 @@
+case name
+
+  when "Dustin"
+    puts "Meh"
+  when "Daniel"
+    puts "Ew"
+  else
+    puts "Nooo"
+end
